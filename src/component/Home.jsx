630cbad9e9e744cdb26110192c44fde0bcc586ec
samiw <sachin.babar07@gmail.com>
2020-10-05 17:17:17 +0530

    remove text

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="text-align home">
       <h4>WELCOME TO</h4>
-      <h3 style={{ marginBottom: '10px' }}>WELCOME TO RENTAL MANAGEMENT SYSTEM</h3>
+      <h3 style={{ marginBottom: '10px' }}>RENTAL MANAGEMENT SYSTEM</h3>
       <span>Please select location</span>
     </div>
   );
